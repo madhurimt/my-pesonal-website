faf647915968f858b4f695b537bf644c9bfd31d3
madhuri maniktahla <madhuri.maniktahla@gmail.com>
2023-08-05 12:12:11 +1000

    Handle event to make website responsive

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function toggleMenu(){
     const ham_menu = document.querySelector(".hamburger-icon");
-    const nav_link= document.querySelectorAll(".nav-links");
+    const nav_link= document.querySelector(".menu-links");
     ham_menu.classList.toggle("open");
     nav_link.classList.toggle("open")
 }
