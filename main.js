0b439a0c3b79583ef4c81b4f02aa6744fc6aaf12
madhuri maniktahla <madhuri.maniktahla@gmail.com>
2023-08-05 11:52:39 +1000

    Design ham_burger menu

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
-const ham_menu = document.getElementsByClassName("hamburger-menu");
-const nav_link= document.getElementsByClassName("nav-links");
-console.log(nav_link);
-
-ham_menu.addEventListener("click",toggleMenu);
-nav_link.addEventListener("click",toggleMenu);
-
 function toggleMenu(){
+    const ham_menu = document.querySelector(".hamburger-icon");
+    const nav_link= document.querySelectorAll(".nav-links");
     ham_menu.classList.toggle("open");
     nav_link.classList.toggle("open")
-
 }
